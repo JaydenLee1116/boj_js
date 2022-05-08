abd00c4dde5146408427246998717d67a447d6db
Jayden Lee(JaeHo Lee) <dltl5@yonsei.ac.kr>
2022-05-08 22:41:07 +0900

    입력 받기 예시, 10950

--- 3level/10950.js
+++ 3level/10950.js
@@ -1,7 +1,7 @@
-import { readFileSync } from 'fs';
+const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 
-const input = readFileSync(filePath).toString().split('\n');
+const input = fs.readFileSync(filePath).toString().split('\n');
 
 const countNum = parseInt(input[0]);
 for (i = 1; i <= countNum; i++) {
