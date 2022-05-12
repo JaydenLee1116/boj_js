67083487a2373b53031dccf4697527034fe97bf4
Jae Ho Lee <dltl5@yonsei.ac.kr>
2022-05-12 18:54:12 +0900

    10951 EOF 개념 잡고 가자

--- 3level/10951.js
+++ 3level/10951.js
@@ -1,16 +1,16 @@
 const fs = require('fs');
 const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 
-const input = fs.readFileSync(path).toString().split('\n');
+const input = fs.readFileSync(path).toString().trim().split('\n');
 
 function solve(input) {
-  for (i = 0; i < input.length; i++) {
+  const count = input.length;
+  for (let i = 0; i < count; i++) {
     let numbers = input[i].split(' ').map(Number);
-    if (numbers[0] === 0 && numbers[1] === 0) {
-      break;
-    }
     console.log(numbers[0] + numbers[1]);
   }
 }
 
 solve(input);
+
+// EOF에 대해서 공부할 것
