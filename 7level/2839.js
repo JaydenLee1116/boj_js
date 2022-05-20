fa1a40ed17bba2bc1cb2159a0ca6e8546b31a1e6
Jayden Lee(JaeHo Lee) <86241737+JaydenLee1116@users.noreply.github.com>
2022-05-20 23:54:06 +0900

    2839(뭔가 억지로 푼 찝찝함)

---
+++ 7level/2839.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const { mainModule } = require('process');
+const path = process.platform === 'linux' ? '/dev/stdin' : './data.txt';
+
+const input = Number(fs.readFileSync(path).toString().trim());
+
+function solve(input) {
+  let count = 0;
+  if (input === 3) {
+    return 1;
+  }
+  if (input < 5 || input === 7) {
+    return -1;
+  }
+  for (let i = 0; i * 3 <= input; i++) {
+    if ((input - i * 3) % 5 === 0) {
+      return i + (input - i * 3) / 5;
+    } else {
+      continue;
+    }
+  }
+}
+
+console.log(solve(input));
