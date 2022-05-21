2b5a96d76b51b9f3cfe3d7879df94b0b1cedf7e4
Jayden Lee(JaeHo Lee) <86241737+JaydenLee1116@users.noreply.github.com>
2022-05-21 14:51:47 +0900

    8level 시작, 1978

---
+++ 8level/1978.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : './data.txt';
+
+const input = fs.readFileSync(path).toString().trim().split('\n');
+
+function solve(input) {
+  let count = Number(input.shift());
+  let numbers = input[0].split(' ').map((x) => Number(x));
+
+  for (let number of numbers) {
+    if (number === 1) {
+      count--;
+    }
+    for (i = 2; i <= number - 1; i++) {
+      if (number % i === 0) {
+        count--;
+        break;
+      } else {
+        continue;
+      }
+    }
+  }
+  return count;
+}
+
+console.log(solve(input));
