da5d2e9c866f9e92eda61dd749458b5273765f0d
Jayden Lee(JaeHo Lee) <86241737+JaydenLee1116@users.noreply.github.com>
2022-05-28 20:56:40 +0900

    10870 피보나치 수정

--- 9level/10870.js
+++ 9level/10870.js
@@ -3,13 +3,9 @@ const path = process.platform === 'linux' ? '/dev/stdin' : './data.txt';
 
 const input = Number(fs.readFileSync(path).toString().trim());
 
-const arr = new Array(input + 1);
-
 const solve = (input) => {
-  if (input === 0) {
-    return 0;
-  } else if (input === 1) {
-    return 1;
+  if (input <= 1) {
+    return input;
   } else {
     return solve(input - 1) + solve(input - 2);
   }
