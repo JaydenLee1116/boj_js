59bfa6095967fe8b08931691fd6c507cfab438f2
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-06-14 21:39:36 +0900

    2751

--- 11level/2750.js
+++ 11level/2750.js
@@ -20,3 +20,34 @@ const solve = (input) => {
 };
 
 solve(input);
+
+/* 버블 소트 구현
+const fs = require('fs');
+const path = process.platform === 'linux' ? '/dev/stdin' : './data.txt';
+
+const input = fs
+  .readFileSync(path)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((number) => parseInt(number));
+
+const solve = (input) => {
+  let count = input.shift();
+  for (let i = count - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (input[j] > input[j + 1]) {
+        let a = input[j];
+        input[j] = input[j + 1];
+        input[j + 1] = a;
+      }
+    }
+  }
+
+  for (let num of input) {
+    console.log(num);
+  }
+};
+
+solve(input);
+*/
