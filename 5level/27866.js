80665f7ae5a2f70a4406bd4a95867add8750508d
JaydenLee1116 <vv55adss@gmail.com>
2023-05-18 08:36:57 +0900

    solve: 5-level 27866

---
+++ 5level/27866.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+// 백준 플랫폼이 리눅스이기 때문. 로컬에서 테스트시 input.txt에 입력받을 내용을 입력 후 파일을 읽어오는식으로 테스트
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
+const input = fs.readFileSync(filePath).toString().split('\n');
+
+// console.log(input); // input 모양 확인 후 주석 처리
+
+function solution(input) {
+  // 풀이 코드 작성
+  const noun = input[0];
+  const n = parseInt(input[1]) - 1;
+  return noun[n];
+}
+
+console.log(solution(input)); // 제출시 주석 풀기
