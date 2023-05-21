166a0d7b6500e15f98a8557c2fb95b4b4cfa14a3
JaydenLee1116 <vv55adss@gmail.com>
2023-05-21 13:31:01 +0900

    solve: 6-level 3003

---
+++ 6level/3003.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+// 백준 플랫폼이 리눅스이기 때문. 로컬에서 테스트시 input.txt에 입력받을 내용을 입력 후 파일을 읽어오는식으로 테스트
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
+const input = fs.readFileSync(filePath).toString().split('\n');
+
+// console.log(input); // input 모양 확인 후 주석 처리
+const CHESS_COUNTS = [1, 1, 2, 2, 2, 8];
+function solution(input) {
+  // 풀이 코드 작성
+  const currentCounts = input[0].split(' ').map((count) => parseInt(count));
+  return CHESS_COUNTS.map((count, i) => count - currentCounts[i]).join(' ');
+}
+
+console.log(solution(input)); // 제출시 주석 풀기
